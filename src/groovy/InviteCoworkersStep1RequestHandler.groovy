5bd8f2af06f4e8db856dda364d47922acda73212
marianosimone <marianosimone@gmail.com>
2012-05-04 12:51:22 -0300

    TRIVIAL adding docs

--- src/groovy/InviteCoworkersStep1RequestHandler.groovy
+++ src/groovy/InviteCoworkersStep1RequestHandler.groovy
@@ -1,31 +1,26 @@
 /**
+ * Tells a user that, as the only member of its organization, it can invite more people to join,
+ * and waits for a response like ok, ask me tomorrow or don't bother me again about it
+ * 
  * @author Leandro Larroulet
  * Date: Sep 13, 2010
  * Time: 13:05
  */
-
 class InviteCoworkersStep1RequestHandler extends RequestHandler {
-
 
     def accepts(Conversation _conversation) {
       boolean accepted = _conversation.context.inviteCoworkers1
       return accepted
     }
 
-
     def doHandle(Conversation _conversation, ChatService _chatService) {
         _conversation.context.clear()
         _conversation.context.inviteCoworkers2=true
 
-        // Sos el �nico usuario en tu compa��a, te gustar�a invitar compa�eros de trabajo?
-        // Si
-        // En otro momento
-        // No, y no me vuelvas a preguntar!
         _conversation.responses << Response.build('sweet.InviteCoworkersStep1RequestHandlerAsk1') //FIXME: Hardcoded humour!
         _conversation.responses << Response.build('sweet.InviteCoworkersStep1RequestHandlerAsk2') //FIXME: Hardcoded humour!
         _conversation.responses << Response.build('sweet.InviteCoworkersStep1RequestHandlerYes') //FIXME: Hardcoded humour!
         _conversation.responses << Response.build('sweet.InviteCoworkersStep1RequestHandlerMaybe') //FIXME: Hardcoded humour!
         _conversation.responses << Response.build('sweet.InviteCoworkersStep1RequestHandlerNo') //FIXME: Hardcoded humour!
-
     }
 }
