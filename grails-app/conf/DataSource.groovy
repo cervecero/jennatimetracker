3263d29bb8bdd66f0db21e2eb6ade36f2d44efa3
marianosimone <marianosimone@gmail.com>
2012-06-07 16:20:39 -0300

    New default DataSource configs

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,18 +1,34 @@
-// Default configuration for the DataSource, using in-memory DB
-// To customize it, copy DataSourcePerEnvironment.groovy.example as DataSourcePerEnvironment.groovy and modify accordingly
+// Default configuration for the DataSource
+// To customize it, copy CusotmDataSource.groovy.example as customconf/CustomDataSource.groovy and modify accordingly
 dataSource {
-    pooled = true
-    driverClassName = 'org.h2.Driver'
-    dbCreate = 'create'
-    url = 'jdbc:h2:mem:devDb'
+    driverClassName = 'com.mysql.jdbc.Driver'
     configClass = org.grails.plugin.hibernate.filter.HibernateFilterDomainConfiguration
+    pooled = true
+    dbCreate = 'update'
 }
 
 hibernate {
+    cache.provider_class = 'org.hibernate.cache.EhCacheProvider'
     cache.use_second_level_cache = true
     cache.use_query_cache = true
-    cache.provider_class = 'org.hibernate.cache.EhCacheProvider'
     show_sql = false
-	dialect = "org.hibernate.dialect.H2Dialect"
+	dialect = 'org.hibernate.dialect.MySQL5InnoDBDialect'
 }
 
+// Making tests run in-memory
+environments {
+    test {
+        dataSource {
+            driverClassName = 'org.h2.Driver'
+            dbCreate = 'create'
+            url = 'jdbc:h2:mem:devDb'
+        }
+        hibernate {
+            cache.use_second_level_cache = true
+            cache.use_query_cache = true
+            cache.provider_class = 'org.hibernate.cache.EhCacheProvider'
+            show_sql = true
+            dialect = "org.hibernate.dialect.H2Dialect"
+        }
+    }
+}
