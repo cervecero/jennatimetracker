731f7530421405c0a2056c82c7609cfaf60c1059
marianosimone <marianosimone@gmail.com>
2012-06-13 12:18:10 -0300

    Just showing a warning when no loading script

--- src/groovy/ComposedConfigScript.groovy
+++ src/groovy/ComposedConfigScript.groovy
@@ -1,3 +1,5 @@
+import org.apache.commons.logging.Log
+import org.apache.commons.logging.LogFactory
 
 /**
  * ConfigScript super class which allows to include pieces of configuration from other files.
@@ -9,11 +11,16 @@
  * see http://naleid.com/blog/2009/07/30/modularizing-groovy-config-files-with-a-dash-of-meta-programming/
  */
 public abstract class ComposedConfigScript extends Script {
-
+    private static Log log = LogFactory.getLog(ComposedConfigScript.class);
+    
     def includeScript(scriptClass) {
-        def scriptInstance = scriptClass.newInstance()
-        scriptInstance.metaClass = this.metaClass
-        scriptInstance.binding = new ConfigBinding(this.getBinding().callable)
-        scriptInstance.&run.call()
+        try {
+            def scriptInstance = scriptClass.newInstance()
+            scriptInstance.metaClass = this.metaClass
+            scriptInstance.binding = new ConfigBinding(this.getBinding().callable)
+            scriptInstance.&run.call()
+        } catch (Exception e) {
+            log.warn("Couldn't load composed config script for class " + scriptClass, e)
+        }
     }
 }
