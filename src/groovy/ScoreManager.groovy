8beecfdeb59cd81376079d1c3e4054b003818240
marianosimone <marianosimone@gmail.com>
2012-06-29 16:18:40 -0300

    CLEANUP Finally removing user points

--- src/groovy/ScoreManager.groovy
+++ src/groovy/ScoreManager.groovy
@@ -7,10 +7,7 @@
 class ScoreManager {
 
   public static void addPoints(User user, String category, String subCategory){
-
     User actualUser = User.get(user.id)
-
-
     def criteria = ScoreCategory.createCriteria()
     def scoreCategory = criteria.get {
       eq("category", category)
@@ -20,22 +17,9 @@ class ScoreManager {
       firstResult(0)
     }
 
-    if (actualUser && scoreCategory){
+    if (actualUser && scoreCategory) {
       user.addToScores(new Score(user: user, company: user.company, date: new Date(), points: scoreCategory.points, scoreCategory: scoreCategory ))
-      if (user.points != null)
-        user.points+= scoreCategory.points
-      else
-        user.points=scoreCategory.points
     }
-  }
-
-  public static int synchronizeUserPoints(User us){
-    User user = User.get(us.id)
-    int points = 0
-    user.scores.each { Score score ->
-      points += score.points
-    }
-    user.points = points
   }
 
 }
